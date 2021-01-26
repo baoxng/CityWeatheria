712649e0450b46e3c073ba92d84bafb4a46970a8
Bao Xiong <bao.xiong2011@gmail.com>
2021-01-25 22:42:23 -0600

    Added API key variable, city, and queryURL link to js file.

---
+++ script.js
@@ -0,0 +1,4 @@
+var APIKey = "d389e89705c7dbbceb75857a1482546f";
+var city= $("#city-input").val();
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
