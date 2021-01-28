ab523213ad01cee33c4ba933da141b7c7a21dbdf
Bao Xiong <bao.xiong2011@gmail.com>
2021-01-27 18:38:06 -0600

    Added ajax call.

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ var APIKey = "d389e89705c7dbbceb75857a1482546f";
 var city= $("#city-input").val();
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+
+$("#submit")
+$.ajax({
+    url: queryUrl,
+    method: 'GET'
+})
+.then(function(response) {
+    var data= response.main
+    console.log(data);
+});
