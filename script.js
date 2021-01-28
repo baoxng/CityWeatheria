c315b24c72bf3f57dbb26d32945be5548ab7eed9
Bao Xiong <bao.xiong2011@gmail.com>
2021-01-27 20:19:12 -0600

    Added pseudo coding for javascript.

--- script.js
+++ script.js
@@ -2,13 +2,36 @@ var APIKey = "d389e89705c7dbbceb75857a1482546f";
 var city= $("#city-input").val();
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+// selected element variables
+var searchBtn= $(".searchBtn");
+var currentDate= $(".current-date");
+var weatherIcon= $(".weather-icon");
+var localStorage= $(".local-storage");
+var temps= $(".temperature");
+var windSpeed= $(".windSpeed");
+var uvIndex= $(".uvIndex");
+var cardRow= $(".card-row");
 
-$("#submit")
-$.ajax({
-    url: queryUrl,
-    method: 'GET'
-})
-.then(function(response) {
-    var data= response.main
-    console.log(data);
-});
+//make a current date
+
+
+//put the search history into local storage
+
+
+//click button event
+
+
+//function to pull weather data
+
+
+//function for city search
+
+
+//Get five day forecast
+
+
+//creating forecast on page
+
+
+
+
