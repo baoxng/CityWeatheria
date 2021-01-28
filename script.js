e78e1f00c0c6b98b278ec0138be598320c0d9662
Bao Xiong <bao.xiong2011@gmail.com>
2021-01-28 16:22:54 -0600

    Added function to get weather.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-var APIKey = "d389e89705c7dbbceb75857a1482546f";
-var city= $("#city-input").val();
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+var city= $("#city-input");
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=d389e89705c7dbbceb75857a1482546f";
 
 // selected element variables
+var nameCity= $(".city-name")
 var searchBtn= $(".searchBtn");
 var currentDate= $(".current-date");
 var weatherIcon= $(".weather-icon");
 var saveStorage= $(".save-storage");
 var temps= $(".temperature");
+var humidity= $(".humidity");
 var windSpeed= $(".windSpeed");
 var uvIndex= $(".uvIndex");
 var cardRow= $(".card-row");
@@ -29,7 +31,7 @@ else{
 }
 
 //Get the local storage data, goes through for loop and adds new city to list with class of savedEntry this gets emptied.
-function getsaveStorage(){
+function getsaveStorage(cityName){
     saveStorage.empty();
     let storageArr=JSON.parse(localStorage.getItem("saveStorage"));
     for (let i= 0; i < storageArr.length; i++){
@@ -41,9 +43,81 @@ function getsaveStorage(){
 
 
 //click button event
-
+searchBtn.on("click", function(s){
+    s.preventDefault();
+    if (city.val() === "") {
+        alert("You must enter a City");
+        return;
+    }
+    console.log("clicked button")
+    getWeather(city.val());
+});
 
 //function to pull weather data
+function pullWeather(cityName, cityTemp,cityHumidity, cityWindSpeed, cityWeatherIcon,uvValu){
+    nameCity.text(cityName);
+    currentDate.text(`(${today}`);
+    temps.text(`Temperature:${cityTemp}\u00B0 F`);
+    humidity.text(`Humidity: ${cityHumidity}%`);
+    windSpeed.text(`Wind Speed: ${cityWindSpeed} MPH`);
+    uvIndex.text(`UV Index: ${uvValu}`);
+    weatherIcon.attr("src", cityWeatherIcon);
+}
+
+//get weather data
+function getWeather (cityInput){
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${desiredCity}&APPID=${apiKey}&units=imperial`;
+}
+$.ajax({
+    url:queryURL,
+    method:"GET"
+})
+.then(function(weatherData){
+    let cityObj={
+        cityName: weatherData.name,
+        cityTemp: weatherData.main.temp,
+        cityHumidity: weatherData.wind.speed,
+        cityUVIndex: weatherData.coord,
+        cityWeatherIconName: weatherData.weather[0].icon
+    }
+    let queryURL= `https://api.openweathermap.org/data/2.5/uvi?lat=${cityObj.cityUVIndex.lat}&lon=${cityObj.cityUVIndex.lon}&APPID=${apiKey}&units=imperial`
+$.ajax({
+    url:queryURL,
+    method:"GET"
+})
+.then(function(uvData){
+    if(JSON.parse(localStorage.getItem("saveStorage")) == null){
+        let saveStorageArr= [];
+        if (saveStorageArr.indexOf(cityObj.cityName) === -1){
+            saveStorageArr.push(cityObj.cityName);
+            localStorage.setItem("saveStorage", JSON.stringify(saveStorageArr));
+            let generatedWeatherIcon = `https:///openweathermap.org/img/w/${cityObj.cityWeatherIconName}.png`;
+            weatherData(cityObj.cityName, cityObj.cityTemp, cityObj.cityHumidity, cityObj.cityWindSpeed, generatedWeatherIcon, uvData.vaule);
+            weatherData(cityObj.cityName);
+        }
+        else{
+            console.log("City already in saveStorage. Unable to add to saved list.")
+            let generatedWeatherIcon= `https:///openweathermap.org/img/w/${cityObj.cityWeatherIconName}.png`;
+            weatherData(cityObj.cityName, cityObj.cityTemp, cityObj.cityHumidity, cityObj.cityWindSpeed, generatedWeatherIcon, uvData.value);
+        }}
+        else {
+            let saveStorageArr =JSON.parse(localStorage.getItem("saveStorage"));
+            if (saveStorageArr.indexOf(cityObj.cityName) === -1){
+                saveStorageArr.push(cityObj.cityName);
+                localStorage.setItem("saveStorage", JSON.stringify(saveStorageArr));
+                let generatedWeatherIcon= `https:///openweathermap.org/img/w/${cityObj.cityWeatherIconName}.png`;
+                weatherData(cityObj.cityName, cityObj.cityTemp, cityObj.cityHumidity, cityObj.cityWindSpeed, generatedWeatherIcon, uvData.value);
+                getsaveStorage(cityObj.cityName);
+            }
+            else{
+                console.log("City already in saveStorage. Unable to add to saved list.")
+                let generatedWeatherIcon= `https:///openweathermap.org/img/w/${cityObj.cityWeatherIconName}.png`;
+                weatherData(cityObj.cityName, cityObj.cityTemp, cityObj.cityHumidity, cityObj.cityWindSpeed,generatedWeatherIcon, uvData.value);
+            }
+        }
+    }
+})
+})
 
 
 //function for city search
